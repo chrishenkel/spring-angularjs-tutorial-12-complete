63ad4bbdf37d5f4c8f2f5899ab9e189f9c193e8d
Chris Henkel <chrishenkel2@gmail.com>
2014-10-19 16:51:49 -0400

    end commit

---
+++ src/main/java/tutorial/core/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package tutorial.core.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Chris on 10/19/14.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
+                .and().withUser("admin").password("password").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+    }
+}
--- src/main/java/tutorial/rest/mvc/AccountController.java
+++ src/main/java/tutorial/rest/mvc/AccountController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import tutorial.core.models.entities.Account;
@@ -44,6 +45,7 @@ public class AccountController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<AccountListResource> findAllAccounts(@RequestParam(value="name", required = false) String name) {
         AccountList list = null;
         if(name == null) {
